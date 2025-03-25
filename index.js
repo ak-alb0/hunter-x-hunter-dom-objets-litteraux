7f5e1e40ff7f67ef498dde8a497ae2671e0592d8
andi-kelmendi <andi-kelmendi@hotmail.com>
2025-03-25 14:10:49 +0100

    feat(index) commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const number = document.querySelector(".number")
+const titre = document.querySelector(".titre")
+const rarete = document.querySelector(".rarete")
+
+let cardA = {
+    number : 1039,
+    titre : "ACCOMPANY",
+    rarete : "F-130",
+}
+
+number.textContent = cardA.number;
+titre.textContent = cardA.titre
+rarete.textContent = cardA.rarete
+
